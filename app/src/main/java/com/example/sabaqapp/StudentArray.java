c4ac8090980264ed1342988d6e1989a8580b5bff
abdullahsalman <abdullahsalmanmardan@gmail.com>
2023-01-28 20:10:53 +0500

    stud array class updated

--- app/src/main/java/com/example/sabaqapp/StudentArray.java
+++ app/src/main/java/com/example/sabaqapp/StudentArray.java
@@ -1,6 +1,13 @@
 package com.example.sabaqapp;
 
+import java.util.ArrayList;
+
 public class StudentArray
 {
-    
+    ArrayList<Student> stdArray=new ArrayList<>();
+
+    public void AddStudent(Student s)
+    {
+        stdArray.add(s);
+    }
 }
